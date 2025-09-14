ffd6d28879e2022c7959313e4af46d9fa9ffe88c
C0deRhin0 <pauloperez9754@gmail.com>
2025-09-14 09:54:36 +0800

    configfiles

--- next.config.js
+++ next.config.js
@@ -4,7 +4,9 @@ const nextConfig = {
   trailingSlash: true,
   images: {
     unoptimized: true
-  }
+  },
+  devIndicators: false, // disables the bottom-left "compiling/ready" UI
+  allowedDevOrigins: ["192.168.119.1"],
 };
 
 module.exports = nextConfig; 
