b8716b4c220897005cba640561615c35da1157b5
C0deRhin0 <pauloperez9754@gmail.com>
2025-09-14 12:00:03 +0800

    centered

--- next.config.js
+++ next.config.js
@@ -6,7 +6,7 @@ const nextConfig = {
     unoptimized: true
   },
   devIndicators: false, // disables the bottom-left "compiling/ready" UI
-  allowedDevOrigins: ["192.168.119.1"],
+  //allowedDevOrigins: ["192.168.119.1"],
 };
 
 module.exports = nextConfig; 
